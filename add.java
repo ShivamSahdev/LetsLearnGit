95f1efee9b8fb79ee39e077a40259f8ba296b801
ShivamSahdev <shivamsahdevit@gmail.com>
2023-07-30 18:57:41 +0530

    This the modification in add.java to aadd the comment

--- add.java
+++ add.java
@@ -1,6 +1,6 @@
 public class add {
     public static void main(String[] args) {
         int a=2, b=4;
-        System.out.println(a+b);
+        System.out.println("The sum of 2 number"+(a+b));
     }
 }
