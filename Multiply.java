f62795593aa9cb6c680a6923773b44bd75a12c45
ShivamSahdev <shivamsahdevit@gmail.com>
2023-07-30 22:17:42 +0530

    second commit from the shivam/multiply branch

--- Multiply.java
+++ Multiply.java
@@ -3,5 +3,6 @@ public class Multiply{
         int a=20, b=30;
         System.out.println("The multiply of "+a+" X "+b+" = "+(a*b));
         System.out.println("Hope you have a wonderful day");
+        System.out.println("another addition to the shivam/multiply branch");
     }
 }
