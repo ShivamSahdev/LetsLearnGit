98a65146ba2ecf34a377d8a517a638a41af6461d
ShivamSahdev <shivamsahdevit@gmail.com>
2023-07-30 22:09:22 +0530

    This to add the message at the end

---
+++ Multiply.java
@@ -0,0 +1,7 @@
+public class Multiply{
+    public static void main(String[] args) {
+        int a=20, b=30;
+        System.out.println("The multiply of "+a+" X "+b+" = "+(a*b));
+        System.out.println("Hope you have a wonderful day");
+    }
+}
